5238d58415047ee150469cb47249f45a16ec68be
Jim Lemmers <jim.lemmers@gmail.com>
2021-01-28 21:21:32 +0100

    refactor tests to use ConnCase

---
+++ test/surface_font_awesome/icon_test.exs
@@ -0,0 +1,124 @@
+defmodule SurfaceFontAwesome.Components.IconTest do
+  use SurfaceFontAwesome.ConnCase, async: true
+
+  alias SurfaceFontAwesome.Icon
+
+  test "basic icon usage" do
+    html =
+      render_surface do
+        ~H"""
+        <Icon icon="500px" />
+        """
+      end
+
+    assert html =~ """
+           <i class="fas fa-500px"></i>
+           """
+  end
+
+  test "set icon size" do
+    html =
+      render_surface do
+        ~H"""
+        <Icon icon="500px" size="4x"/>
+        """
+      end
+
+    assert html =~ """
+           <i class="fas fa-500px fa-4x"></i>
+           """
+  end
+
+  test "transformations of the icon" do
+    html =
+      render_surface do
+        ~H"""
+        <Icon icon="500px" transform="grow-10 left-20"/>
+        """
+      end
+
+    assert html =~ """
+           <i data-fa-transform="grow-10 left-20" class="fas fa-500px"></i>
+           """
+  end
+
+  test "setting the primary color without duotone icon should be ignored" do
+    html =
+      render_surface do
+        ~H"""
+        <Icon icon="camera" primary_color="blue" />
+        """
+      end
+
+    assert html =~ """
+           <i class="fas fa-camera"></i>
+           """
+  end
+
+  test "setting the primary opacity and secondary color" do
+    html =
+      render_surface do
+        ~H"""
+        <Icon icon="camera" icon_style="duotone" secondary_color="blue" primary_opacity={{0.6}} />
+        """
+      end
+
+    assert html =~ """
+           <i class="fad fa-camera" style="--fa-secondary-color: blue; --fa-primary-opacity: 0.6"></i>
+           """
+  end
+
+  test "masking of icons" do
+    html =
+      render_surface do
+        ~H"""
+        <Icon icon="pencil-alt" transform="shrink-10 up-.5" mask="comment" style="background: MistyRose"/>
+        """
+      end
+
+    assert html =~ """
+           <i data-fa-mask="fas fa-comment" data-fa-transform="shrink-10 up-.5" class="fas fa-pencil-alt" style="background: MistyRose"></i>
+           """
+  end
+
+  test "rotating icons" do
+    html =
+      render_surface do
+        ~H"""
+          <Icon icon="camera" rotate="90" />
+        """
+      end
+
+    assert html =~ """
+           <i class="fas fa-camera fa-rotate-90"></i>
+           """
+  end
+
+  test "flipping icons" do
+    html =
+      render_surface do
+        ~H"""
+          <Icon icon="camera" flip="vertical" />
+        """
+      end
+
+    assert html =~ """
+           <i class="fas fa-camera fa-flip-vertical"></i>
+           """
+  end
+
+  test "flipping and rotating icons" do
+    html =
+      render_surface do
+        ~H"""
+          <Icon icon="camera" flip="vertical" rotate="90"/>
+        """
+      end
+
+    assert html =~ """
+             <span class="fa-flip-vertical">
+             <i class="fas fa-camera fa-rotate-90"></i>
+           </span>
+           """
+  end
+end
---
+++ test/surface_font_awesome/layers_test.exs
@@ -0,0 +1,87 @@
+defmodule SurfaceFontAwesome.LayerTest do
+  use SurfaceFontAwesome.ConnCase, async: true
+
+  alias SurfaceFontAwesome.Icon
+  alias SurfaceFontAwesome.Layers, as: FALayers
+  alias SurfaceFontAwesome.{TextLayer, CounterLayer}
+
+  test "layered icons" do
+    html =
+      render_surface do
+        ~H"""
+        <FALayers>
+          <Icon icon="camera" size="1x" />
+          <Icon icon="ban" size="2x"/>
+        </FALayers>
+        """
+      end
+
+    assert html =~
+             """
+             <span class="fa-layers fa-fw">
+               <i class="fas fa-camera fa-1x"></i>
+               <i class="fas fa-ban fa-2x"></i>
+             </span>
+             """
+  end
+
+  test "layered icons with styles" do
+    html =
+      render_surface do
+        ~H"""
+        <FALayers style="background: blue">
+          <Icon icon="camera" size="1x" />
+          <Icon icon="ban" size="2x"/>
+        </FALayers>
+        """
+      end
+
+    assert html =~
+             """
+             <span class="fa-layers fa-fw" style="background: blue">
+               <i class="fas fa-camera fa-1x"></i>
+               <i class="fas fa-ban fa-2x"></i>
+             </span>
+             """
+  end
+
+  test "add text layer with icons" do
+    html =
+      render_surface do
+        ~H"""
+        <FALayers>
+          <Icon icon="calendar"/>
+          <TextLayer class="fa-inverse" transform="shrink-8 down-3" style="font-weight:900">27</TextLayer>
+        </FALayers>
+        """
+      end
+
+    assert html =~
+             """
+             <span class="fa-layers fa-fw">
+               <i class="fas fa-calendar"></i>
+               <span data-fa-transform="shrink-8 down-3" class="fa-layers-text fa-inverse" style="font-weight: 900">27</span>
+             </span>
+             """
+  end
+
+  test "add counter layer" do
+    html =
+      render_surface do
+        ~H"""
+        <FALayers>
+          <Icon icon="calendar"/>
+          <CounterLayer position="top-right" transform="shrink-8 down-3" style="font-weight:900">27</CounterLayer>
+        </FALayers>
+        """
+      end
+
+    assert html =~
+             """
+             <span class="fa-layers fa-fw">
+               <i class="fas fa-calendar"></i>
+               <span data-fa-transform="shrink-8 down-3" class="fa-layers-counter fa-layers-top-right" style="font-weight: 900">27</span>
+             </span>
+             """
+  end
+end
