620173e069a0735ac963b80925496fc96365b456
Jim Lemmers <jim.lemmers@gmail.com>
2021-01-28 21:20:19 +0100

    update to surface 0.2.0

--- mix.exs
+++ mix.exs
@@ -10,6 +10,7 @@ defmodule SurfaceFontAwesome.MixProject do
       version: @version,
       elixir: "~> 1.8",
       description: "FontAwesome implemented with Surface components",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs(),
@@ -29,7 +30,7 @@ defmodule SurfaceFontAwesome.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:surface, "~> 0.1.0"},
+      {:surface, "~> 0.2.0"},
       {:floki, ">= 0.27.0", only: :test},
       {:ex_doc, ">= 0.19.0", only: :dev, runtime: false}
     ]
@@ -42,4 +43,7 @@ defmodule SurfaceFontAwesome.MixProject do
       source_url: @source_url
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
