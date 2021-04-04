9264cbf627beacc6190acc4d293d5c9a0baf1b94
Aleksandr Lossenko <aleksandr.lossenko@gmail.com>
2021-04-04 22:25:42 +0200

    bump surface to ~> 0.3.0

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule SurfaceFontAwesome.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:surface, "~> 0.2.0"},
+      {:surface, "~> 0.3.0"},
       {:surface_formatter, git: "https://github.com/surface-ui/surface_formatter"},
       {:floki, ">= 0.27.0", only: :test},
       {:ex_doc, ">= 0.19.0", only: :dev, runtime: false}
