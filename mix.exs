db834b5b6ea09b1f5c42027d3ea4ac1af9dfe4a4
Jim Lemmers <jim.lemmers@gmail.com>
2021-10-27 13:30:58 +0200

    bump version to 0.2.4

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule SurfaceFontAwesome.MixProject do
   use Mix.Project
 
-  @version "0.2.3"
+  @version "0.2.4"
   @source_url "https://github.com/Zurga/surface_font_awesome"
 
   def project do
